94c0747a236c5d145a55aa10781bc0ee32f3df7f
anilozdem <anil.ozdem2134@gmail.com>
2024-12-30 12:37:03 +0300

    anil v2

--- src/OrtakClass.java
+++ src/OrtakClass.java
@@ -1,5 +1,6 @@
 public class OrtakClass {
     public static void main(String[] args) {
         System.out.println("Buradan babayigit geçti");
+        System.out.println("Ortak Class Anil");
     }
 }
